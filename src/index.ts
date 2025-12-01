8bfa46f6307f3e72041bc29f9d79a27226ae0cdd
kiranbalaji71 <kiran19701@gmail.com>
2025-12-01 20:46:05 +0530

    feat: Configure CORS middleware with specific options

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // ✅ allow all origins
+    methods: ["POST", "GET"], // allow required methods
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 const createEmailTemplate = (name: string, email: string, message: string) => {
